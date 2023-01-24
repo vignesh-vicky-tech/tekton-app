692a923ca5fa64c2bcd521c671f4b42d606ef0e9
vignesh-vicky-tech <85112276+vignesh-vicky-tech@users.noreply.github.com>
2023-01-24 12:36:37 +0530

    Create ExampleWebApplication.java

---
+++ source/java/com/example/ExampleWebApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point web application
+ */
+@SpringBootApplication
+public class ExampleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleWebApplication.class, args);
+    }
+}
